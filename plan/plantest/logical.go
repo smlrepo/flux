e8cd235d56ba3cc625ba09da2ea24b979a25e2d3
He Liu <liulonnie@gmail.com>
2018-10-08 12:41:54 -0700

    use uuid.Must

--- plan/plantest/logical.go
+++ plan/plantest/logical.go
@@ -6,9 +6,5 @@ import (
 )
 
 func RandomProcedureID() plan.ProcedureID {
-	id, err := uuid.NewV4()
-	if err != nil {
-		panic(err)
-	}
-	return plan.ProcedureID(id)
+	return plan.ProcedureID(uuid.Must(uuid.NewV4()))
 }
